e7277b389b823e467f829b05b4ddecd82405474d
Niv Kaminer <niv.kaminer@campus.technion.ac.il>
2023-02-24 21:31:20 +0200

    change CodeWord to Word and change Code to be an ndarray

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ from typing import List, Set
 import numpy as np
 from numpy.typing import NDArray
 
-CodeWord = NDArray[np.int_]
-Code = List[CodeWord]
+Word = NDArray[np.uint8] # 1 dimentional binary array
+Code = NDArray[np.uint8] # 2 dimentional binary array, representing multiple words
 
 # NIV
 
@@ -19,17 +19,17 @@ def AllWord(n: int) -> Set[CodeWord]:
 
 # return every word in the insertion in the insertion ball of `word`.
 # i.e. words that are created by inserting a single bit.
-def WordInsertionBall(word: CodeWord) -> Set[CodeWord]:
+def WordInsertionBall(word: Word) -> Set[Word]:
     return
 
 # get the insertion ball of all words in the code togather
-def CodeInsertionBall(code: List[CodeWord]) -> Set[CodeWord]:
+def CodeInsertionBall(code: List[Word]) -> Set[Word]:
     return
 
 # FCFS
 
 # exhastive search for words not covered by codes
-def getWordsNotCovered(words: Set[CodeWord], codes: List[Code]) -> Set[CodeWord]:
+def getWordsNotCovered(words: Set[Word], codes: List[Code]) -> Set[Word]:
     return
 
 # get words not covered by VT0, and those not covered by both VT0 and VT((n+1)/2)

e7eb4d63c66cce2dab7648e477bed9639b93ddc2
Niv Kaminer <niv.kaminer@campus.technion.ac.il>
2023-02-24 21:33:27 +0200

    implement creating codes (using only numpy)

--- main.py
+++ main.py
@@ -1,20 +1,37 @@
 from typing import List, Set
 import numpy as np
 from numpy.typing import NDArray
+import itertools
 
 Word = NDArray[np.uint8] # 1 dimentional binary array
 Code = NDArray[np.uint8] # 2 dimentional binary array, representing multiple words
 
 # NIV
 
-# return a list of every word in the code VT_a(n)
+def compute_syndrome(code: Code) -> Word:
+    """
+    compute the VT code syndrome for each element in the code
+    """
+    n = code.shape[1]
+    return np.mod(np.sum((1+np.arange(n))*code, axis=1),n+1)
+
 def VTCodeGenerator(a: int, n: int) -> Code:
-    return
+    """
+    return a list of every word in the code VT_a(n)
+    """
+    all_words = AllWords(n)
+    VT_filter = compute_syndrome(all_words) == a
+    # returns all words whose syndrome is a (as per the definition)
+    return all_words[VT_filter]
 
-# return all words of length n
-def AllWord(n: int) -> Set[CodeWord]:
-    return
-
+def AllWords(n: int) -> Code:
+    """
+    return every word of length n
+    """
+    all_combinations = itertools.product([0, 1], repeat=n)
+    flattened = itertools.chain.from_iterable(all_combinations)
+    array = np.fromiter(flattened, dtype=np.uint8, count=(n*2**n))
+    return array.reshape(-1, n)
 # EDEN
 
 # return every word in the insertion in the insertion ball of `word`.

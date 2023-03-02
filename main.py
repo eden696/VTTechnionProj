e7cd9f45fbc9bb130e8436191e108cbd32131dcb
Niv Kaminer <niv.kaminer@campus.technion.ac.il>
2023-03-02 21:33:18 +0200

    remove unused find_code_insertion_ball

--- main.py
+++ main.py
@@ -107,47 +107,6 @@ def get_all_words(n: int) -> Code:
     array = np.fromiter(flattened, dtype=np.uint8, count=(n*2**n))
     return array.reshape(-1, n)
 
-def find_code_insertion_ball(code: Code) -> Code:
-    """
-    compute the insertion ball for each word in the code,
-    i.e. words that are created by inserting a single bit,
-    returning all words in the union of the balls
-    """
-    length = code.shape[1]
-    word_count = code.shape[0]
-    # for each word, before index idx, insert the inverse bit at idx
-    inv_code = 1 - code
-    # except at the end where both 0 and 1 can be appended
-    appended = np.repeat([[0,1]], word_count, axis=0)
-    values = np.append(inv_code, appended, axis=1)
-
-    # the size of an insertion ball for n length word is n+2,
-    ball_size = length + 2
-
-    # create n+2 instances of each word,
-    # so that the insertions can be done independantly for each one
-    repeated = np.repeat(code, ball_size, axis=0)
-
-    # flatten all instances of each word,
-    # so that insertions can be performed on uniform indicies
-    flattened = repeated.reshape((word_count, -1))
-
-    # each instance except the last inserts at an index greater by 1,
-    # than its predecessor. calculate where that would be in the flattened array
-    indicies = (np.arange(ball_size)*length) + np.arange(ball_size)
-    # the last instance inserts in the same position as the last,
-    # so undo the increment
-    indicies[-1] = indicies[-1] - 1
-
-    inserted = np.insert(flattened, indicies, values, axis=1)
-
-    # reshape the result of the insertion into words of length n+1,
-    # which can have duplicates across different words in the code
-    duplicated = inserted.reshape((-1, length+1))
-
-    # deduplicate the above words and return the resulting code
-    return np.unique(duplicated, axis=0)
-
 def collect_coset_coverage(n: int) -> Tuple[Word, Word]:
     """
     returns the cosets used during the greedy algorithm,
